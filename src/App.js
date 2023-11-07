26d289dbb9168880b3aee78191f3bce606c378e5
khanMahmood68 <mahmoodashrafkhanm0@gmail.com>
2023-11-07 23:58:35 +0530

    creating chating app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  onChildAdded,
+  addCommentElement,
+} from "firebase/database";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [chats, setChats] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const db = getDatabase();
+  const chatListRef = ref(db, "chats");
+
+  useEffect(() => {
+    onChildAdded(chatListRef, (data) => {
+      setChats((chats) => [...chats, data.val()]);
+    });
+  }, []);
+
+  const sendChat = () => {
+    const chatRef = push(chatListRef);
+    set(chatRef, {
+      name,
+      message: message,
+    });
+    // // const c = [...chats,{ name: name, message: message }];
+
+    // // OR
+
+    // const c = [...chats];
+    // c.push({ name: name, message: message });
+    // setChats(c);
+    setMessage("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {name ? null : (
+        <div>
+          <input
+            type="text"
+            placeholder="Enter name to start chat"
+            onBlur={(e) => setName(e.target.value)}
+          />
+        </div>
+      )}
+
+      {name ? (
+        <div>
+          <h3>User:{name}</h3>
+          <div className="chat-container">
+            {chats.map((c, i) => (
+              <div
+                key={i}
+                className={`container ${c.name === name ? "me" : ""}`}
+              >
+                <p className="chatbox">
+                  <strong>{c.name}:</strong>
+                  <span>{c.message}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="btm">
+            <input
+              type="text"
+              value={message}
+              placeholder="Enter your chat"
+              onInput={(e) => setMessage(e.target.value)}
+            />
+            <button onClick={(e) => sendChat()}>Send</button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDUNBTJoF3Ia-DBFlfqfxP4xnnqPhkXTbA",
+  authDomain: "react-chat-app-e01b7.firebaseapp.com",
+  databaseURL: "https://react-chat-app-e01b7-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-e01b7",
+  storageBucket: "react-chat-app-e01b7.appspot.com",
+  messagingSenderId: "944678901625",
+  appId: "1:944678901625:web:2307f7966aeab33231c1a8",
+  measurementId: "G-KVETJCW2DW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
